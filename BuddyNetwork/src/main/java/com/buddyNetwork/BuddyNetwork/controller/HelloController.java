78fe6f515dae7d75b69904c52d2f63b56cbd3464
rksvw <ritik.vw98@gmail.com>
2025-11-23 19:48:58 +0530

    get mapping and route id path  variable

--- BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/controller/HelloController.java
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.buddyNetwork.BuddyNetwork.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
@@ -11,9 +14,9 @@ public class HelloController {
         return "Hello Java Backend";
     }
 
-    @GetMapping("/name")
-    public String getMethodName() {
-        return new String("<h1>Hello World! My name is Ritik Sharma.</h1>");
+    @GetMapping("/id/{id}")
+    public String getMethodName(@PathVariable("id") Long userId) {
+        return String.format("<h1>Hello World! My name is %d.</h1>", userId);
     }
 
 }
