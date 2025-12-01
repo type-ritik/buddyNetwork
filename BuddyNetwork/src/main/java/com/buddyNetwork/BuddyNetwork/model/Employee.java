05c7057001894a680486e330c40f35623673e6b3
rksvw <ritik.vw98@gmail.com>
2025-12-01 19:18:46 +0530

    custom serialization and deserialization

--- BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/model/Employee.java
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/model/Employee.java
@@ -1,14 +1,33 @@
 package com.buddyNetwork.BuddyNetwork.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// Ignore any property with Null values
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Employee {
+
+    // Change the name of the 'id' property to 'employeeId'
+    @JsonProperty("employeeId")
     private Long id;
+
+    @JsonProperty("employeeName")
     private String name;
+
+    // Ignore the 'department' property
+    @JsonIgnore
     private String department;
 
     public Employee(Long id, String name, String department) {
         this.id = id;
         this.name = name;
         this.department = department;
+    }
+
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Employee() {
