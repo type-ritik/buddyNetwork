8a6ce7412d906e99d6fdfec43ffd44c8a4cd9c23
rksvw <ritik.vw98@gmail.com>
2025-12-22 21:01:11 +0530

    permit all request map

--- BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/config/SecurityConfig.java
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +14,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.csrf(customizer -> customizer.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 };
