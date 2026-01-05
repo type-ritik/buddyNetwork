2ebdc92426aa13a058a3311ffd49660c9e440a6e
rksvw <ritik.vw98@gmail.com>
2026-01-05 14:33:46 +0530

    implement resource and response not found exception

---
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/utility/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.buddyNetwork.BuddyNetwork.utility;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/utility/ResponseNotFoundException.java
@@ -0,0 +1,8 @@
+
+package com.buddyNetwork.BuddyNetwork.utility;
+
+public class ResponseNotFoundException extends RuntimeException {
+    public ResponseNotFoundException(String message) {
+        super(message);
+    }
+}
