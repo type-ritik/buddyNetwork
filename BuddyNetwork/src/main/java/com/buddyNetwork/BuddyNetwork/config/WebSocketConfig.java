370298c5aeddaff7462b77acfdbdb79b3ca4b5f3
rksvw <ritik.vw98@gmail.com>
2026-01-08 16:03:04 +0530

    Socket configuration endpoint

--- BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/config/WebSocketConfig.java
+++ BuddyNetwork/src/main/java/com/buddyNetwork/BuddyNetwork/config/WebSocketConfig.java
@@ -1,23 +1,24 @@
 package com.buddyNetwork.BuddyNetwork.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.buddyNetwork.BuddyNetwork.component.SocketConnectionHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketConnectionHandler(), "/hello").setAllowedOrigins("*");
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-    }
+    // @Override
+    // public void configureMessageBroker(MessageBrokerRegistry registry) {
+    // registry.setApplicationDestinationPrefixes("/app");
+    // registry.enableSimpleBroker("/topic");
+    // }
 }
